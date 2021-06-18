37961a11922aa158abe3c87452b81a900fd54bda
Paracells <enoalife@hotmail.com>
2021-06-18 10:06:39 +0300

    added sharing

--- src/components/header/TheHeader.vue
+++ src/components/header/TheHeader.vue
@@ -260,15 +260,6 @@ export default {
   }
 }
 
-.fade-enter-active, .fade-leave-active {
-  transition: opacity .5s;
-}
-
-.fade-enter, .fade-leave-to /* .fade-leave-active до версии 2.1.8 */
-{
-  opacity: 0;
-}
-
 @media (max-width: 575.98px) {
   .header {
     &__center-text {
